ac9f3b5ac5dc7687e53a8f41d69f08324d8ab875
xiaolong <longjiuwei999@163.com>
2024-06-13 17:05:48 +0800

    add solana wallet functions

--- crates/bot-api/src/router/wallet.rs
+++ crates/bot-api/src/router/wallet.rs
@@ -5,3 +5,15 @@ pub async fn wallet() -> &'static str {
     // user_id
     "wallet"
 }
+
+// create solana wallet
+pub async fn create_wallet() {}
+
+// remove solana wallet
+pub async fn remove_wallet() {}
+
+// set default wallet
+pub async fn set_default_wallet() {}
+
+// import wallet
+pub async fn import_wallet() {}
