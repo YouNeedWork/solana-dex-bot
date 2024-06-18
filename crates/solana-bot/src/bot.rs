aae7acc29261a28abd666313d9f50013ed4f3d65
YusongWang <admin@wangyusong.com>
2024-06-18 10:32:58 -0800

    add db to bot

--- crates/solana-bot/src/bot.rs
+++ crates/solana-bot/src/bot.rs
@@ -3,33 +3,49 @@ use std::error::Error;
 use teloxide::prelude::*;
 use teloxide::types::InlineKeyboardButton;
 use teloxide::types::InlineKeyboardMarkup;
-use teloxide::types::InlineQueryResultArticle;
-use teloxide::types::InputMessageContent;
-use teloxide::types::InputMessageContentText;
 use teloxide::types::Me;
 use teloxide::utils::command::BotCommands;
 use teloxide::Bot;
 use tracing::info;
 
+use diesel::pg::PgConnection;
+use diesel::r2d2::{ConnectionManager, Pool};
+use std::sync::Arc;
+
+type PGPool = Pool<ConnectionManager<PgConnection>>;
+
 pub struct SolanaBot {
     admin_id: i32,
     bot: Bot,
+    db: PGPool,
 }
 
 impl SolanaBot {
-    pub fn new(token: String) -> Result<Self> {
+    pub fn new(token: String, db: PGPool) -> Result<Self> {
         let bot = teloxide::Bot::new(token);
 
-        Ok(Self { bot, admin_id: 0 })
+        Ok(Self {
+            bot,
+            admin_id: 0,
+            db,
+        })
     }
 
     pub async fn run(self) -> Result<()> {
-        let SolanaBot { bot, admin_id: _ } = self;
+        let SolanaBot {
+            bot,
+            admin_id: _,
+            db,
+        } = self;
+
+        let db = Arc::new(db);
 
         let handler = dptree::entry()
-            .branch(Update::filter_message().endpoint(message_handler))
-            .branch(Update::filter_callback_query().endpoint(callback_handler))
-            .branch(Update::filter_inline_query().endpoint(inline_query_handler));
+            .branch(
+                Update::filter_message()
+                    .endpoint(move |bot, msg, me| message_handler(bot, msg, me, db.clone())),
+            )
+            .branch(Update::filter_callback_query().endpoint(callback_handler));
 
         Dispatcher::builder(bot, handler)
             .enable_ctrlc_handler()
@@ -44,28 +60,30 @@ impl SolanaBot {
 #[derive(BotCommands)]
 #[command(rename_rule = "lowercase")]
 enum Command {
-    /// Display this text
     Help,
-    /// Start
     Start,
-    Buy,
-    Sell,
+    BuySell,
+    Menu,
     Seting,
     Lang,
 }
 
-pub async fn start(bot: Bot, id: ChatId) -> Result<()> {
-    let keyboard = InlineKeyboardMarkup::new(vec![vec![
-        InlineKeyboardButton::callback("Help".to_string(), "help".to_string()),
-        InlineKeyboardButton::callback("Ping".to_string(), "ping".to_string()),
-        InlineKeyboardButton::callback("Buy".to_string(), "buy".to_string()),
-        InlineKeyboardButton::callback("Start".to_string(), "start".to_string()),
-        InlineKeyboardButton::callback("Sell".to_string(), "sell".to_string()),
-    ]]);
+pub async fn menu(bot: Bot, id: ChatId) -> Result<()> {
+    let keyboard = InlineKeyboardMarkup::new(vec![
+        vec![
+            InlineKeyboardButton::callback("Buy/Sell".to_string(), "BuySell".to_string()),
+            InlineKeyboardButton::callback("Assets".to_string(), "Assets".to_string()),
+        ],
+        vec![InlineKeyboardButton::callback(
+            "Help".to_string(),
+            "help".to_string(),
+        )],
+    ]);
 
     bot.send_message(id, "Let's start my frrend!".to_string())
         .reply_markup(keyboard)
         .await?;
+
     Ok(())
 }
 
@@ -73,6 +91,7 @@ async fn message_handler(
     bot: Bot,
     msg: Message,
     me: Me,
+    db: Arc<PGPool>,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
     if let Some(text) = msg.text() {
         match BotCommands::parse(text, me.username()) {
@@ -81,44 +100,20 @@ async fn message_handler(
                     .await?;
             }
             Ok(Command::Start) => {
-                // TODO: query default wallet
-                info!("Who send msg: {}", me.user.id);
-
-                // Create a list of buttons and send them.
-                let keyboard = make_keyboard();
-                bot.send_message(msg.chat.id, "Debian versions:")
-                    .reply_markup(keyboard)
-                    .await?;
+                //TODO if user not register then register and generate wallet for it
+                menu(bot, msg.chat.id).await?;
             }
-            Ok(Command::Buy) => {
-                // Create a list of buttons and send them.
-                let keyboard = make_keyboard();
-                bot.send_message(msg.chat.id, "Debian versions:")
-                    .reply_markup(keyboard)
-                    .await?;
+            Ok(Command::Menu) => {
+                //  TODO: query default wallet
+                // info!("Who send msg: {}", me.user.id);
+                menu(bot, msg.chat.id).await?;
             }
-            Ok(Command::Sell) => {
-                // Create a list of buttons and send them.
-                let keyboard = make_keyboard();
-                bot.send_message(msg.chat.id, "Debian versions:")
-                    .reply_markup(keyboard)
-                    .await?;
-            }
-            Ok(Command::Seting) => {
-                // Create a list of buttons and send them.
-                let keyboard = make_keyboard();
-                bot.send_message(msg.chat.id, "Debian versions:")
-                    .reply_markup(keyboard)
-                    .await?;
-            }
-            Ok(Command::Lang) => {
-                // Create a list of buttons and send them.
-                let keyboard = make_keyboard();
-                bot.send_message(msg.chat.id, "Debian versions:")
-                    .reply_markup(keyboard)
-                    .await?;
-            }
+            Ok(Command::BuySell) => {}
+            Ok(Command::Seting) => {}
+            Ok(Command::Lang) => {}
             Err(_) => {
+                info!(text, "Got text");
+
                 bot.send_message(msg.chat.id, "Command not found!").await?;
             }
         }
@@ -127,54 +122,16 @@ async fn message_handler(
     Ok(())
 }
 
-async fn inline_query_handler(
-    bot: Bot,
-    q: InlineQuery,
-) -> Result<(), Box<dyn Error + Send + Sync>> {
-    let choose_debian_version = InlineQueryResultArticle::new(
-        "0",
-        "Chose debian version",
-        InputMessageContent::Text(InputMessageContentText::new("Debian versions:")),
-    )
-    .reply_markup(make_keyboard());
-
-    bot.answer_inline_query(q.id, vec![choose_debian_version.into()])
-        .await?;
-
-    Ok(())
-}
-
-/// Creates a keyboard made by buttons in a big column.
-fn make_keyboard() -> InlineKeyboardMarkup {
-    let mut keyboard: Vec<Vec<InlineKeyboardButton>> = vec![];
-
-    let root_keyboard = ["Buy", "Sell"];
-
-    for key in root_keyboard.chunks(3) {
-        let row = key
-            .iter()
-            .map(|&version| InlineKeyboardButton::callback(version.to_owned(), version.to_owned()))
-            .collect();
-
-        keyboard.push(row);
-    }
-
-    InlineKeyboardMarkup::new(keyboard)
-}
-
-/// When it receives a callback from a button it edits the message with all
-/// those buttons writing a text with the selected Debian version.
-///
-/// **IMPORTANT**: do not send privacy-sensitive data this way!!!
-/// Anyone can read data stored in the callback button.
 async fn callback_handler(bot: Bot, q: CallbackQuery) -> Result<(), Box<dyn Error + Send + Sync>> {
-    if let Some(version) = q.data {
-        let text = format!("You chose: {version}");
-
-        // Tell telegram that we've seen this query, to remove 🕑 icons from the
-        // clients. You could also use `answer_callback_query`'s optional
-        // parameters to tweak what happens on the client side.
+    if let Some(chose) = q.data {
+        log::info!("You chose: {}", chose);
         bot.answer_callback_query(q.id).await?;
+
+        let text = if chose == "Buy" {
+            "You chose to buy Debian!"
+        } else {
+            "You chose to sell Debian!"
+        };
 
         // Edit text of the message to which the buttons were attached
         if let Some(Message { id, chat, .. }) = q.message {
@@ -182,8 +139,6 @@ async fn callback_handler(bot: Bot, q: CallbackQuery) -> Result<(), Box<dyn Erro
         } else if let Some(id) = q.inline_message_id {
             bot.edit_message_text_inline(id, text).await?;
         }
-
-        log::info!("You chose: {}", version);
     }
 
     Ok(())
--- crates/solana-bot/src/main.rs
+++ crates/solana-bot/src/main.rs
@@ -2,16 +2,13 @@
 extern crate rust_i18n;
 
 use anyhow::Result;
-use solana_client::nonblocking::rpc_client::RpcClient;
-use solana_core::trade::Trade;
-use solana_sdk::{
-    signature::{Keypair, Signer},
-    signer::SeedDerivable,
-};
+use clap::Parser;
 
+use diesel::pg::PgConnection;
+use diesel::r2d2::{ConnectionManager, Pool};
+
+use config::Config;
 use rust_i18n::t;
-use solana_sdk::pubkey::Pubkey;
-use std::str::FromStr;
 
 mod bot;
 mod config;
@@ -20,7 +17,15 @@ rust_i18n::i18n!("locales");
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    //let key = Keypair::new();
+    let config = Config::parse();
+    tracing_subscriber::fmt::init();
+
+    let manager = ConnectionManager::<PgConnection>::new(config.database_url);
+
+    let db = diesel::r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool");
+    /*
     let wallet = Keypair::from_seed(&[
         118, 206, 164, 217, 88, 74, 225, 36, 231, 186, 155, 160, 221, 19, 71, 28, 253, 155, 196,
         38, 231, 56, 108, 80, 34, 160, 46, 147, 98, 213, 233, 119,
@@ -33,6 +38,7 @@ async fn main() -> Result<()> {
 
     let client = RpcClient::new("https://alien-winter-orb.solana-mainnet.quiknode.pro/9c31f4035d451695084d9d94948726ea43683107/".to_string());
     let balances = client.get_balance(&wallet.pubkey()).await.unwrap();
+
     println!("balance: {:?} SOL ", balances as f64 / 1_000_000_000 as f64);
 
     let input_token_mint = Pubkey::from_str("E3ZELac8ywEmt5WL5WVncrCXPePSoZuwaQ7rqJDTxs8M")?;
@@ -40,7 +46,7 @@ async fn main() -> Result<()> {
     let amount = trade.get_spl_balance(&input_token_mint).await?;
 
     println!("balance: {:?} ", amount);
-
+    */
     println!("{}", t!("hello"));
     println!("{}", t!("hello", locale = "zh-CN"));
 
@@ -56,5 +62,7 @@ async fn main() -> Result<()> {
         .await?;
     */
 
+    let bot = bot::SolanaBot::new(config.telegram_token, db)?;
+    bot.run().await?;
     Ok(())
 }
