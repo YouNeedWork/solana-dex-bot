4984c08ba92e43cfe33d181f531ecdf4c3641319
YusongWang <admin@wangyusong.com>
2024-06-18 07:14:02 -0800

    remove api mode

--- crates/bot-api/src/main.rs
+++
@@ -1,8 +0,0 @@
-mod open_telemetry;
-mod router;
-
-#[tokio::main]
-async fn main() {
-    open_telemetry::subscriber_telemetry();
-    router::start_server().await;
-}
--- crates/bot-api/src/router/orders.rs
+++
@@ -1,7 +0,0 @@
-use tracing::instrument;
-#[instrument(name = "orders")]
-pub async fn orders() -> &'static str {
-    //req: user_id
-    //res: orders table data
-    "orders"
-}
