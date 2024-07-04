1978109b562cee16e282c213e15a8a79ea4e269b
YusongWang <admin@wangyusong.com>
2024-07-04 05:08:14 -0800

    update

--- crates/solana-trade/src/trade.rs
+++ crates/solana-trade/src/trade.rs
@@ -4,27 +4,46 @@ use trade_proto::{
     trade_service_server::TradeService, TradeReply, TradeRequest,
 };
 
+use anyhow::Result;
+use tokio::sync::mpsc::{channel, Receiver, Sender};
+
 pub mod trade_proto {
     tonic::include_proto!("trade");
 }
 
-#[derive(Debug, Default)]
-pub struct Trade {}
+struct Work((String, TradeRequest));
+
+#[derive(Clone)]
+pub struct Trade {
+    work_sender: Sender<Work>,
+}
 
 impl Trade {
     pub fn new() -> Self {
-        Self {}
+        let (tx, rx) = channel::<Work>(1000);
+
+        tokio::spawn(async move { Self::swap_work(rx).await });
+
+        Self { work_sender: tx }
     }
 }
 
 //TODO: Add one channel for do the trade onchain. and wait for success
-/*
 impl Trade {
-    async fn swap_work(&self) -> Result<()> {
+    async fn swap_work(mut works: Receiver<Work>) -> Result<()> {
+        while let Some(work) = works.recv().await {
+            let Work((id, request)) = work;
+            info!("handle request id: {} user_id {:?}", id, request);
+
+            //TODO: query database
+            //TODO: do the trade onchain
+            //TODO: save the result to database
+            //TODO: return the result
+        }
+
         Ok(())
     }
 }
-*/
 
 #[tonic::async_trait]
 impl TradeService for Trade {
@@ -32,12 +51,22 @@ impl TradeService for Trade {
         &self, request: Request<TradeRequest>,
     ) -> Result<Response<TradeReply>, Status> {
         info!("Got a request: {:?}", request);
+        //TODO: check all params.
+        let id = uuid::Uuid::new_v4().to_string();
+
+        self.work_sender
+            .send(Work((id.clone(), request.into_inner())))
+            .await
+            .unwrap();
 
         let reply = TradeReply {
             code: 0,
-            message: format!("Hello From Grpc!"),
+            message: id,
         };
 
         Ok(Response::new(reply)) // Send back our formatted greeting
     }
 }
+
+#[tokio::test]
+async fn test_trade() {}
